226e4892ae238139709734e0b17150f688a9bda1
Chad Rutherford <c.ruth544@gmail.com>
2015-11-21 23:13:16 -0800

    win condition set up, now need to set up locking of boxes based on prior move

--- script/tic-tac-toe.js
+++ script/tic-tac-toe.js
@@ -23,38 +23,97 @@
       }
     }
   }
-  function fillCell (cell) {
+
+  var board;
+
+  function fillCell (cell, player, ultimateId, smallId) {
     var cellText = currentTurn.getPlayer().letter;
     cell.innerHTML = cellText;
-    // var cellText = document.createTextNode(currentTurn.getPlayer().letter);
-    // cell.appendChild(cellText);
     cell.style.color = currentTurn.getPlayer().color;
-    var arrayToPush = cell.getAttribute('id').replace(/i/g, '');
-    currentTurn.getPlayer().filledCells.push(arrayToPush);
-    currentTurn.changeTurns();
+
+    //fill board obj
+    if (player === playerX) {
+      board[ultimateId][smallId.charAt(0)][smallId.charAt(1)]++;
+    } else {
+      board[ultimateId][smallId.charAt(0)][smallId.charAt(1)]--;
+    }
+
+    // var arrayToPush = cell.getAttribute('id').replace(/i/g, '');
+    // currentTurn.getPlayer().filledCells.push(arrayToPush);
+  }
+
+  function storeCell (cell, player, ultimateId, smallId) {
+    if (player[ultimateId]) {
+      player[ultimateId].push(smallId);
+    } else {
+      player[ultimateId] = [smallId];
+    }
   }
 
   //winning logic
-  function checkWin (cell) {
-    var id = cell.getAttribute('id');
-    var ultimateId = id.replace(/i/g, '').split('/')[0].split('');
-    var smallId = id.split('/')[1].split('');
-    console.log(smallId, ultimateId);
+  function checkWin (cell, player, ultimateId, smallId) {
+    if (player.filledCells[ultimateId].length > 2) {
+      if (playerX.squaresWon.indexOf(ultimateId) === -1 && playerO.squaresWon.indexOf(ultimateId) === -1) {
+        if (loopCheck(player.filledCells[ultimateId])) {
+          console.log(player.letter, "wins!");
+          player.squaresWon.push(ultimateId);
+          //call function to show change
+          return loopCheck(player.squaresWon);
+        }
+      }
+    }
+  }
+
+  function loopCheck (playerArray) {
+    //first check for diagonals
+    if (playerArray.length > 2) {
+      if (playerArray.indexOf('11') > -1) {
+        if (playerArray.indexOf('00') > -1 && playerArray.indexOf('22') > -1) {
+          return true;
+        } else if (playerArray.indexOf('20') > -1 && playerArray.indexOf('02') > -1) {
+          return true;
+        }
+      }
+    }
+
+    //next check if any row has 3
+    var countFirst  = [0,0,0];
+    var countSecond = [0,0,0];
+    for (var i = 0; i < playerArray.length; i++) {
+      countFirst[Number(playerArray[i].charAt(0))]++;
+      countSecond[Number(playerArray[i].charAt(1))]++;
+    }
+
+    if (countFirst.indexOf(3) > -1 || countSecond.indexOf(3) > -1) {
+      return true;
+    }
+    return false;
   }
 
 //player parent
   function Player (letter, color) {
     this.letter = letter;
     this.color = color;
-    this.filledCells = [];
+    this.filledCells = {};
+    this.squaresWon = [];
   }
 
   //what happens when a cell is clicked
   function cellWasClicked (cell) {
     if (!cell.innerHTML) {
+      var player = currentTurn.getPlayer();
+      var id = cell.getAttribute('id');
+      var ultimateId = id.replace(/i/g, '').split('/')[0];
+      var smallId = id.split('/')[1];
       // console.log(cell.getAttribute("id"), "was clicked")
-      fillCell(cell);
-      checkWin(cell);
+      fillCell(cell, player, ultimateId, smallId);
+      storeCell(cell, player.filledCells, ultimateId, smallId);
+      if (checkWin(cell, player, ultimateId, smallId)) {
+        console.log(player.letter, 'WINS THE GAME!');
+      }
+
+
+      currentTurn.changeTurns();
     }
   }
 
@@ -89,6 +148,8 @@
         var row = document.createElement("tr");
         row.setAttribute("class", "cellRow");
 
+        board[ultimateHashIndex.join('')].push([]);
+
         for (var j = 0; j < 3; j++) {
           // Create a <td> element and a text node, make the text
           // node the contents of the <td>, and put the <td> at
@@ -99,9 +160,10 @@
           cell.setAttribute("class", "cell");
           cell.setAttribute("id", cellID);
           cell.setAttribute("onClick", "cellWasClicked(this)")
-          // cell.addEventListener("click", function () {
-          //   onClickEvent(this.cell);
-          // })
+
+          // set up board obj
+          board[ultimateHashIndex.join('')][i].push(0);
+
           cell.appendChild(cellText);
           row.appendChild(cell);
         }
@@ -131,6 +193,7 @@
       var ultimateTbl = document.createElement("table");
       ultimateTbl.setAttribute("id", "ultimateTable");
       var ultimateTblBody = document.createElement("tbody");
+      board = {};
 
       for (var i = 0; i < 3; i++) {
         var ultimateRow = document.createElement("tr");
@@ -139,6 +202,7 @@
 
           var ultimateCell = document.createElement("td");
           var ultimateCellID = 'i'+i+'-'+j;
+          board[''+i+j] = [];
           ultimateCell.setAttribute("class", "ultimateCell");
           ultimateCell.setAttribute("id", ultimateCellID);
           basicHash(ultimateCell, [i,j]);
@@ -150,6 +214,7 @@
 
       ultimateTbl.appendChild(ultimateTblBody);
       main.appendChild(ultimateTbl);
+
     }
     if (flag) {
       ultimateHash();
@@ -176,10 +241,30 @@ setUpBoard(true);
 
 
 
+  // function checkWin (cell, player, ultimateId, smallId) {
+  //   if (playerX.squaresWon.indexOf(ultimateId) === -1 && playerO.squaresWon.indexOf(ultimateId) === -1) {
+  //     if (player.filledCells[ultimateId].length > 2) {
+  //       var countFirst  = [0,0,0];
+  //       var countSecond = [0,0,0];
+  //       for (var i = 0; i < player.filledCells[ultimateId].length; i++) {
+  //         countFirst[Number(player.filledCells[ultimateId][i].charAt(0))]++;
+  //         countSecond[Number(player.filledCells[ultimateId][i].charAt(1))]++;
+  //       }
 
-
-
-
+  //       if (countFirst.indexOf(3) > -1 || countSecond.indexOf(3) > -1) {
+  //         player.squaresWon.push(ultimateId);
+  //         console.log(player.letter, "wins!");
+  //       }
+  //       for (var i = 0; i < countFirst.length; i++) {
+  //         countFirst[i]--;
+  //         countSecond[i]--;
+  //       }
+  //       if (countFirst.indexOf(0) > -1 && countSecond.indexOf(0) > -1 && countFirst.indexOf(-1) < 0 && countSecond.indexOf(-1) < 0 && player.filledCells[ultimateId].indexOf('11') > -1) {
+  //         console.log(player.letter, "wins!");
+  //       }
+  //     }
+  //   }
+  // }
 
 
 
